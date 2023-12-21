24118852fdf561ac8eaf38e64888dc0581bbf83f
dudnjsckrgo <dudnjsckrgo@gmail.com>
2023-12-21 17:43:12 +0900

    aiffel7 라이브러리

---
+++ setup_aiffel.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiffel7',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple Python library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='aiffel7_그루',
+    author_email='your.email@example.com',
+    url='https://github.com/dudnjsckrgo/aiffel7_library',
+    license='LICENSE',
+    install_requires=[
+        # 필요한 의존성 패키지 나열
+    ],
+    classifiers=[
+        # PyPI 분류자
+        # 참조: https://pypi.org/classifiers/
+    ],
+)

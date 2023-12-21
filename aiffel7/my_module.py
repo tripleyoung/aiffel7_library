fac3842ffcf277e87dd960312f5dc48d6a941cd9
dudnjsckrgo <dudnjsckrgo@gmail.com>
2023-12-21 18:02:01 +0900

    module 이제부터 채워야됨

---
+++ aiffel7/my_module.py
@@ -0,0 +1,9 @@
+# my_module.py
+
+def greet(name):
+    """ 사용자에게 인사말을 출력하는 함수 """
+    return f"Hello, {name}!"
+
+def add(a, b):
+    """ 두 숫자의 합을 반환하는 함수 """
+    return a + b
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiffel7',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     description='A simple Python library',
     long_description=open('README.md').read(),

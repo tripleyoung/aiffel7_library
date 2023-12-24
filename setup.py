47b1896a319ba7d75d52fc4fbe619c5c4877b110
dudnjsckrgo <dudnjsckrgo@gmail.com>
2023-12-24 16:22:30 +0900

    setup 파일에 의존성 추가

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiffel7',
-    version='0.1.1',
+    version='0.2.2',
     packages=find_packages(),
     description='A simple Python library',
     long_description=open('README.md').read(),
@@ -13,6 +13,8 @@ setup(
     license='LICENSE',
     install_requires=[
         # 필요한 의존성 패키지 나열
+        'beautifulsoup4',  # This is the package name for bs4
+        'requests'
     ],
     classifiers=[
         # PyPI 분류자
